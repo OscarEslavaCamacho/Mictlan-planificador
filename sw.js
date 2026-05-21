93c44433cea532ac033bd6a7a526ee6b6ac64afe
OscarEslavaCamacho <88334637+OscarEslavaCamacho@users.noreply.github.com>
2026-05-21 10:09:36 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'planificador-v2';
-const ASSETS = ['./index.html','./manifest.json'];
+const CACHE = 'planificador-v3';
+const ASSETS = ['./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
